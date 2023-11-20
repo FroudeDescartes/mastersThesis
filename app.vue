8a6aa5164bcceab79343ad01bebae1b07ea806f1
Filip <dr.ipi95@gmail.com>
2023-11-20 19:03:38 +0100

    Added Header, Footer and Dark mode

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
-  <div> 
-    <AppHeader />
-    <ContentDoc />
-    <AppFooter />
+  <div class="flex flex-col h-screen justify-between"> 
+    <MainHeader />
+    <NuxtPage class="mb-auto m-4"/>
+    <MainFooter />
   </div>
 </template>
